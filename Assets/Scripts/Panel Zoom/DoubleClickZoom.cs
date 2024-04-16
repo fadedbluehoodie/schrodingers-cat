08d2fc600aba4dc766f1f8f5c176ac6e27e976a8
sturtevg <gabrielsturtevan@u.boisestate.edu>
2024-04-16 16:31:19 -0600

    Page Zoom Arrows now working
    The sample panel now zooms correctly with both clicking on and arrow navigation. Panel 6 doesn't work for clicking, might need adjusting in the FindPos function, but otherwise works.

--- Assets/Scripts/Panel Zoom/DoubleClickZoom.cs
+++ Assets/Scripts/Panel Zoom/DoubleClickZoom.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class DoubleClickZoom : MonoBehaviour, IPointerClickHandler
 {
@@ -11,9 +13,54 @@ public class DoubleClickZoom : MonoBehaviour, IPointerClickHandler
     Vector2 referencePoint = new Vector2(0.5f, 0.5f); // Center of the panel
     Vector2 originalSize;
 
+    RectTransform rt;
+    Vector2 newSize;
+
+    List<GameObject> positions = new List<GameObject>(); //List of child game objects (pannels)
+    int currentPannel = -1; //currentPannel being -1 means no pannel is currently selected
+
     void Start()
     {
         originalSize = GetComponent<RectTransform>().sizeDelta;
+        rt = GetComponent<RectTransform>();
+        newSize =  new Vector2(rt.rect.width * scaleFactor, rt.rect.height * scaleFactor);
+        
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            positions.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(currentPannel != -1)
+        {
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentPannel++;
+                if(currentPannel <= positions.Count) //If there is a pannel available to go to
+                {
+                    FindPos(Vector2.zero, currentPannel);
+                }
+                else //If there isn't
+                {
+                    ZoomOut();
+                }
+            }
+
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentPannel--;
+                if(currentPannel > 0) //If there is a pannel available to go to
+                {
+                    FindPos(Vector2.zero, currentPannel);
+                }
+                else //If there isn't
+                {
+                    ZoomOut();
+                }
+            }
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -22,13 +69,13 @@ public class DoubleClickZoom : MonoBehaviour, IPointerClickHandler
         {
             if (!zoomedIn)
             {
-                ZoomIn(eventData.position);
+                FindPos(eventData.position, currentPannel);
             }
             else
             {
                 ZoomOut();
+                zoomedIn = false;
             }
-            zoomedIn = !zoomedIn;
             lastClickTime = 0;
         }
         else
@@ -37,24 +84,78 @@ public class DoubleClickZoom : MonoBehaviour, IPointerClickHandler
         }
     }
 
-    void ZoomIn(Vector2 doubleClickPosition)
+    void FindPos(Vector2 doubleClickPosition, int curPan)
     {
-        RectTransform rt = GetComponent<RectTransform>();
-        Vector2 referenceOffset = new Vector2(
-            (referencePoint.x - 0.5f) * rt.rect.width,
-            (referencePoint.y - 0.5f) * rt.rect.height
-        );
-        Vector2 newSize = new Vector2(rt.rect.width * scaleFactor, rt.rect.height * scaleFactor);
-        Vector2 localReferencePoint = new Vector2(
-            (doubleClickPosition.x - rt.position.x) / rt.rect.width,
-            (doubleClickPosition.y - rt.position.y) / rt.rect.height
-        );
-        Vector2 offset = new Vector2(
-            (localReferencePoint.x * newSize.x) - referenceOffset.x,
-            (localReferencePoint.y * newSize.y) - referenceOffset.y
-        );
-        rt.sizeDelta = newSize;
-        rt.localPosition -= (Vector3)offset;
+        if(curPan == -1) //If no pannel is currently selected
+        {
+            for (int i = 0; i < positions.Count; i++)
+            {
+                var posit = positions[i];
+                var positRect = Rect.MinMaxRect(                    //Get's the bounds for the pannel size
+                    posit.transform.position.x - (posit.GetComponent<SpriteRenderer>().bounds.size.x / 2),
+                    posit.transform.position.y - (posit.GetComponent<SpriteRenderer>().bounds.size.x / 2),
+                    posit.transform.position.x + (posit.GetComponent<SpriteRenderer>().bounds.size.x / 2),
+                    posit.transform.position.y + (posit.GetComponent<SpriteRenderer>().bounds.size.x / 2)
+                );
+                
+                if(positRect.Contains(doubleClickPosition)) //If the click is within the bounds
+                {
+                    ZoomIn(posit.transform.position);
+                    currentPannel = i + 1;
+                    Debug.Log(currentPannel);
+                }
+            }
+        }
+        else if(curPan != -1) //If a pannel is already selected
+        {
+            Debug.Log(curPan);
+            Debug.Log(positions[curPan - 1]);
+            var posit = positions[curPan - 1].transform.position;
+            ZoomIn(posit);
+        }
+    }
+
+    void ZoomIn(Vector2 newPos)
+    {
+        if(zoomedIn) //If the player is already zoomed in, adjust values.
+        {
+            Vector2 referenceOffset = new Vector2(
+                (referencePoint.x - 0.5f) * rt.rect.width,
+                (referencePoint.y - 0.5f) * rt.rect.height
+            );
+            Vector2 localReferencePoint = new Vector2(
+                (newPos.x - rt.position.x) / rt.rect.width,
+                1.5f * ((newPos.y - rt.position.y) / rt.rect.height)
+            );
+            Vector2 offset = new Vector2(
+                (localReferencePoint.x * newSize.x) - referenceOffset.x,
+                (localReferencePoint.y * newSize.y) - referenceOffset.y
+            );
+            rt.sizeDelta = newSize;
+            rt.localPosition = localReferencePoint;
+            rt.localPosition -= (Vector3)offset;
+            zoomedIn = true;
+        }
+        else //If player isn't already zoomed in, DON'T adjust values
+        {
+            Vector2 referenceOffset = new Vector2(
+                (referencePoint.x - 0.5f) * rt.rect.width,
+                (referencePoint.y - 0.5f) * rt.rect.height
+            );
+            Vector2 localReferencePoint = new Vector2(
+                (newPos.x - rt.position.x) / rt.rect.width,
+                (newPos.y - rt.position.y) / rt.rect.height
+            );
+            Vector2 offset = new Vector2(
+                (localReferencePoint.x * newSize.x) - referenceOffset.x,
+                (localReferencePoint.y * newSize.y) - referenceOffset.y
+            );
+            rt.sizeDelta = newSize;
+            rt.localPosition = localReferencePoint;
+            rt.localPosition -= (Vector3)offset;
+            zoomedIn = true;
+        }
+        
     }
 
     void ZoomOut()
@@ -62,5 +163,6 @@ public class DoubleClickZoom : MonoBehaviour, IPointerClickHandler
         RectTransform rt = GetComponent<RectTransform>();
         rt.sizeDelta = originalSize;
         rt.localPosition = Vector3.zero;
+        currentPannel = -1;
     }
 }
