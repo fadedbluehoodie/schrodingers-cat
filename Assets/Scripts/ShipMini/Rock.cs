1baf8071bec6987574cf9226276eacc067ff1a2d
acrandell <acgamer1999@gmail.com>
2024-04-26 15:29:51 -0600

    stuff

--- Assets/Scripts/ShipMini/Rock.cs
+++ Assets/Scripts/ShipMini/Rock.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rock : MonoBehaviour
 {
     public PlayerController1 PC1;
     public PlayerController2 PC2;
+    public string sceneToLoad; // Name of the scene to load after destroying the rock
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,7 +21,17 @@ public class Rock : MonoBehaviour
         // Check if both players have used their tools before destroying the rock
         if (PC1.destroy1 && PC2.destroy2)
         {
-            Destroy(gameObject);
+            // Destroy the rock
+            DestroyRock();
         }
+    }
+
+    void DestroyRock()
+    {
+        // Destroy the rock GameObject
+        Destroy(gameObject);
+
+        // Load the new scene
+        SceneManager.LoadScene(4);
     }
 }
